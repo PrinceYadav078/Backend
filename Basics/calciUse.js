45be4c0597191a9127320a317c7532047043ff29
Prince Yadav <prince.yadav@brainwaveconsulting.co.in>
2024-03-13 04:43:31 +0530

    implement import and exports write and exports some calci methods in a file and import in a another file and use it

---
+++ Basics/calciUse.js
@@ -0,0 +1,8 @@
+const myCalci= require('./calci') // here you have imported your module
+
+// use your imported method like this
+
+myCalci.addition(10,50)
+myCalci.substract(50,40)
+myCalci.multiply(5,4)
+myCalci.division(80,6)
