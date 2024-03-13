c332cd53962168365ad0988823952ffd39d058f9
Prince Yadav <prince.yadav@brainwaveconsulting.co.in>
2024-03-14 00:48:12 +0530

    create dynamic route by using route parameters

--- Express/simpleServer.js
+++ Express/simpleServer.js
@@ -3,6 +3,7 @@ const app = express()
 
 //Middleware => it is a function which runs before route 
 //if u want to do anything before routing then you can use middleware
+
 app.use((req, res, next)=>{
     console.log("hello i am from middleware 1")
     next()
@@ -20,4 +21,35 @@ app.get('/', function (req, res) {
 app.get('/profile', function (req, res) {
     res.send('Hello from profile route')
   })
+
+app.get('/contact',(req,res)=>{
+  res.send("Hello from contact route")
+})
+
+//req=> req me data hota hai aane bale user ki request ki taraf ka 
+//      like uski location ,device info and other things 
+
+//res=> res me control hote hai jinke basis pe ham server se response send kar 
+//      sakte hai 
+
+// next=> next is just a push so that our request moves to the next thing which should be executed. 
+
+// app.get('/contact/prince',(req,res)=>{
+//   res.send("Hello from prince")
+// })
+
+// app.get('/contact/ganesh',(req,res)=>{
+//   res.send("Hello from ganesh")
+// })
+
+// app.get('/contact/tushar',(req,res)=>{
+//   res.send("Hello from tushar")
+// })
+
+//Route Parameters=> so istead of creating route for every user we can create dynamic route by using colon (:)
+// and after that give name like username and to access that value use req.params.username
+app.get('/contact/:username',(req,res)=>{
+  res.send(`Hello from ${req.params.username}`)
+})
+
 app.listen(3000)
