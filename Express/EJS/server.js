03a01e136ab4045858fae2ae5a9223206e1c1f03
Prince Yadav <prince.yadav@brainwaveconsulting.co.in>
2024-03-14 04:46:08 +0530

    use error handler

--- Express/EJS/server.js
+++ Express/EJS/server.js
@@ -12,4 +12,17 @@ app.get('/about',(req,res)=>{
     res.render("about", {age:25})
 })
 
+app.get('/error',(req,res)=>{
+    throw Error("Sorry! Page not found")
+})
+
+app.use(function errorHandler (err, req, res, next) {
+    if (res.headersSent) {
+      return next(err)
+    }
+    res.status(500)
+    res.render('error', { error: err })
+  })
+
+
 app.listen(5000);
