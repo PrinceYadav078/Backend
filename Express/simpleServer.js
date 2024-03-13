5d4c3ce1f7c230cb0bdd754d10f3ff59024951ed
Prince Yadav <prince.yadav@brainwaveconsulting.co.in>
2024-03-13 05:57:55 +0530

    use middleware

--- Express/simpleServer.js
+++ Express/simpleServer.js
@@ -1,6 +1,18 @@
 const express = require('express')
 const app = express()
 
+//Middleware => it is a function which runs before route 
+//if u want to do anything before routing then you can use middleware
+app.use((req, res, next)=>{
+    console.log("hello i am from middleware 1")
+    next()
+})
+
+app.use((req, res, next)=>{
+    console.log("hello i am from middleware 2")
+    next()
+})
+
 app.get('/', function (req, res) {
   res.send('Hello I am learning Express')
 })
