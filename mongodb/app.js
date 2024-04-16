66db5e9c4d768a59d393063b603b57bc66667f1e
Prince Yadav <prince.yadav@brainwaveconsulting.co.in>
2024-04-16 12:57:24 +0530

    session creation reading and deletion

--- mongodb/app.js
+++ mongodb/app.js
@@ -6,12 +6,19 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var session= require('express-session')
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+app.use(session({
+  resave:false,
+  saveUninitialized:false,
+  secret:"vk18"
+}));
 
 app.use(logger('dev'));
 app.use(express.json());
--- mongodb/routes/index.js
+++ mongodb/routes/index.js
@@ -30,4 +30,29 @@ router.get('/delete',async function(req,res){
   let deletedUser=await userModel.findOneAndDelete({username:"princeyadav18"})
   res.send(deletedUser)
 })
+
+//Session cretion
+router.get('/createsession',function(req,res){
+  req.session.ban=true;
+  res.send("session created succesfully")
+})
+
+//Session delete
+router.get('/removesession', function(req, res){
+  req.session.destroy(function(err){
+    if(err){
+      throw err;
+    }
+  })
+  res.send("session deleted")
+})
+
+//session reading
+router.get('/checksession', function(req, res){
+  if(req.session.ban === true){
+    res.send("You Are Banned");
+  }else{
+    res.send("You are not banned")
+  }
+})
 module.exports = router;
